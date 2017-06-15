82a6ef1f5bfa41dcb9e60734cbdf251a4354a9f9
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-15 15:49:09 -0700

    Insert Query added

--- interface/User.py
+++ interface/User.py
@@ -29,6 +29,6 @@ class Users:
             return False
 
     def registration(self, user_and_pass):
-        self.c.execute(INSERT_USER, (user_and_pass[0], user_and_pass[1]))
+        self.c.execute(INSERT_USER, [user_and_pass[0], user_and_pass[1], ])
         self.SharedVariables.database.db.commit()
         return self.is_user(user_and_pass)
