abf11e8c372ac8001df557521f7244e47183314f
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-15 13:58:46 -0700

    Added visualization for movie projections

--- interface/Movie.py
+++ interface/Movie.py
@@ -1,5 +1,6 @@
 from database.queries.insert_queries import INSERT_MOVIE
-from database.queries.select_queries import SELECT_MOVIES_ORDERED_BY_RATING
+from database.queries.select_queries import SELECT_MOVIES_ORDERED_BY_RATING,\
+                                            SELECT_PROJECTION_FOR_MOVIE
 
 from settings.SharedVariables import SharedVariables
 from prettytable import PrettyTable
@@ -31,9 +32,25 @@ class Movies:
         except Exception:
             print("Database not initilized or connected")
 
+    @staticmethod
+    def movie_projections(id):
+        try:
+            db_wrapper = SharedVariables.database
+            c = db_wrapper.get_cursor()
+            data = c.execute(SELECT_PROJECTION_FOR_MOVIE, [id, ])
+            db_wrapper.get_db().commit()
+
+            t = PrettyTable(SharedVariables.projection_col)
+            for row in data:
+                t.add_row([row[0], row[1], row[2], row[3], (100 - row[4])])
+            return str(t)
+        except Exception:
+            print("Database not initilized or connected")
+
 
 if __name__ == '__main__':
     from database.connection.database_connection import Database
     SharedVariables.database = Database()
     Movies.add_movie("Baywatch", 10)
     print(Movies())
+    print(Movies.movie_projections(1))
