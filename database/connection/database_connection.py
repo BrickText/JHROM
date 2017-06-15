e5b925dbea8c0716003a13f12a5ebd1c3222a8a6
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-15 12:13:10 -0700

    addind database

--- database/connection/database_connection.py
+++ database/connection/database_connection.py
@@ -1,14 +1,35 @@
 import sqlite3
 
 from settings.database_settings import DB_NAME
+from database.queries.create_table_queries import *
 
 
-class DatabaseConnection:
+class Database:
 
-    def __init__(self):
-        self.db = sqlite3.connect(DB_NAME)
+    def __init__(self, database_name=DB_NAME):
+        self.db_name = DB_NAME
+        self.create_connection()
+
+    def create_connection(self):
+        self.db = sqlite3.connect(self.db_name)
         self.db.row_factory = sqlite3.Row
-        self.c = self.db.cursor()
+        self.cursor = self.db.cursor()
+
+    def drop_tables(self):
+        self.cursor.execute(DROP_MOVIE_TABLE)
+        self.cursor.execute(DROP_USER_TABLE)
+        self.cursor.execute(DROP_RESETVATION_TABLE)
+        self.cursor.execute(DROP_PROJECTION_TABLE)
+
+    def create_tables(self):
+        self.cursor.execute(CREATE_MOVIE_TABLE)
+        self.cursor.execute(CREATE_USER_TABLE)
+        self.cursor.execute(CREATE_PROJECTION_TABLE)
+        self.cursor.execute(CREATE_RESERVATION_TABLE)
+        self.db.commit()
 
     def get_cursor(self):
-        return self.c
+        return self.cursor
+
+    def get_db(self):
+        return self.db
