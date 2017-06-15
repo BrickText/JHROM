01acbd6b59f2ae5c72a994b23757233ed8468d33
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-15 12:41:28 -0700

    interface/Projections.py

---
+++ interface/Projections.py
@@ -0,0 +1,23 @@
+from database.queries.insert_queries import INSERT_PROJECTION
+from settings.SharedVariables import SharedVariables
+
+
+class Projection():
+    def __init__(self, movie_id, projection_date):
+        pass
+
+    @classmethod
+    def add_projection(cls, movie_id, movie_type, date):
+        try:
+            db_wrapper = SharedVariables.database
+            c = db_wrapper.get_cursor()
+            c.execute(INSERT_PROJECTION, [movie_id, movie_type, date, ])
+            db_wrapper.get_db().commit()
+        except Exception:
+            print("Database not initialized")
+
+
+if __name__ == '__main__':
+    from database.connection.database_connection import Database
+    SharedVariables.database = Database()
+    Projection.add_projection(1, "2D", "2018-08-11 11:11:11")
