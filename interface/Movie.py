c7d5efc182cada1236b775bde722d0eed1e5f532
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-15 15:01:17 -0700

    Added delete

--- interface/Movie.py
+++ interface/Movie.py
@@ -1,7 +1,9 @@
 from database.queries.insert_queries import INSERT_MOVIE
 from database.queries.update_queries import UPDATE_MOVIE
+from database.queries.delete_queries import DELETE_MOVIE
 from database.queries.select_queries import SELECT_MOVIES_ORDERED_BY_RATING,\
-                                            SELECT_PROJECTION_FOR_MOVIE
+                                            SELECT_PROJECTION_FOR_MOVIE, \
+                                            SELECT_MOVIE_BY_ID
 
 from settings.SharedVariables import SharedVariables
 from prettytable import PrettyTable
@@ -24,11 +26,36 @@ class Movies:
         return str(t)
 
     @staticmethod
+    def get_movie(id):
+        try:
+            db_wrapper = SharedVariables.database
+            c = db_wrapper.get_cursor()
+            data = c.execute(SELECT_MOVIE_BY_ID, [id, ])
+            db_wrapper.get_db().commit()
+        except Exception:
+            print("Database not initilized or connected")
+
+        t = PrettyTable(SharedVariables.movie_col)
+        for row in data:
+            t.add_row([row[0], row[1], row[2]])
+        return str(t)
+
+    @staticmethod
     def add_movie(name, rating):
         try:
             db_wrapper = SharedVariables.database
             c = db_wrapper.get_cursor()
             c.execute(INSERT_MOVIE, [name, rating, ])
+            db_wrapper.get_db().commit()
+        except Exception:
+            print("Database not initilized or connected")
+
+    @staticmethod
+    def delete_movie(id):
+        try:
+            db_wrapper = SharedVariables.database
+            c = db_wrapper.get_cursor()
+            c.execute(DELETE_MOVIE, [id, ])
             db_wrapper.get_db().commit()
         except Exception:
             print("Database not initilized or connected")
@@ -63,6 +90,4 @@ if __name__ == '__main__':
     from database.connection.database_connection import Database
     SharedVariables.database = Database()
     Movies.add_movie("Baywatch", 10)
-    print(Movies())
-    Movies.update_movie(1, "Baywatch(1)", 8.7)
-    print(Movies())
+    print(Movies.get_movie(2))
