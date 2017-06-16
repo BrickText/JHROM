4f43bab83f4f3aae7fdf002799a516a5c3939887
ivanmilevtues <ivanmilevtues@gmail.com>
2017-06-16 10:51:54 +0300

    Added commiting and executemany queries

--- database/connection/execute_query.py
+++ database/connection/execute_query.py
@@ -1,12 +1,15 @@
 from settings.SharedVariables import SharedVariables
 
 
-def execute_query(query, args, commit=False):
+def execute_query(query, args, commit=False, execute_many=False):
     db_wrapper = SharedVariables.database
     c = db_wrapper.get_cursor()
+    if execute_many:
+        c.executemany(query, args)
+    else:
+        data = c.execute(query, args)
+
     if commit:
-        c.execute(query, args)
         db_wrapper.get_db().commit()
         return True
-    else:
-        return c.execute(query, args)
+    return data
