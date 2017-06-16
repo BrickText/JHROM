1554827a5c085ec4afc63c746618cc9f773c62bd
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-06-16 11:21:44 +0300

    Update help options

--- settings/SharedVariables.py
+++ settings/SharedVariables.py
@@ -8,8 +8,8 @@ class SharedVariables:
     options = ['show movies', 'show movie projection <movieid> '
                '[<projection date>]', 'make reservation', 'add movie',
                'add projection', 'update movie', 'update projection',
-               'delete movie', 'delete projection', 'update reservation',
-               'reset database' 'help', 'exit']
+               'delete movie', 'delete projection', 'delete reservation',
+               'reset database', 'help', 'exit']
 
     available_seat = '.'
     taken_seat = 'x'
